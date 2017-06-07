58e3a7b3359517934ad3b950980dd66aa54798fc
Mikael Kalms <mikael@kalms.org>
2017-06-07 16:19:06 +0200

    Hardcode region / zone to us-central1-a; locate gcloud on cmdline

--- Backend/commands/create_gcloud_configuration.sh
+++ Backend/commands/create_gcloud_configuration.sh
@@ -2,13 +2,20 @@
 
 # Reference: https://chengl.com/working-with-multiple-projects-on-gke/
 
-if [ $# -lt 4 ]; then
-  echo "Usage: create_gcloud_configuration.sh <configuration name> <project name> <region> <zone>"
+# Hardcoded region & zone to us-central1-a since currently (2017-06-07) Google Cloud Functions give
+#   strange errors during deploy/run if run from other regions than us-central1
+
+gcloud=`./commands/gcloud.sh`
+
+region="us-central1"
+zone="us-central1-a"
+
+if [ "$#" -lt 2 ]; then
+  echo "Usage: create_gcloud_configuration.sh <configuration name> <project name>"
   exit 1
 fi 
 
-gcloud config configurations create $1
-gcloud config set project $2
-#gcloud config set functions/region $3
-gcloud config set compute/region $3
-gcloud config set compute/zone $4
+"$gcloud" config configurations create "$1"
+"$gcloud" config set project "$2"
+"$gcloud" config set compute/region "$region"
+"$gcloud" config set compute/zone "$zone"
